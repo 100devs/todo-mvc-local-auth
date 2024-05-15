1342a448990da7c995475d42472623311ab8798b
Renna Carver <87547832+rennacarver@users.noreply.github.com>
2024-05-15 13:16:01 -0700

    locate error in auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -12,23 +12,22 @@ const User = require('../models/User')
   }
   
   exports.postLogin = (req, res, next) => {
+    
     const validationErrors = []
     if (!validator.isEmail(req.body.email)) validationErrors.push({ msg: 'Please enter a valid email address.' })
     if (validator.isEmpty(req.body.password)) validationErrors.push({ msg: 'Password cannot be blank.' })
-  
+      
     if (validationErrors.length) {
       req.flash('errors', validationErrors)
-      //debug console statement
-      console.log('validation errors')
       return res.redirect('/login')
     }
     req.body.email = validator.normalizeEmail(req.body.email, { gmail_remove_dots: false })
   
     passport.authenticate('local', (err, user, info) => {
-      if (err) { return next(err) }
+      
+      if (err) { 
+        return next(err) } //error is occuring on this line
       if (!user) {
-        //console debug statement
-        console.log('user not found?')
         req.flash('errors', info)
         return res.redirect('/login')
       }
