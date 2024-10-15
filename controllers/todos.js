6fdfc4793cfceeb1fd65f8dfef724c855cb9f418
Nancy Hunter <hunternancym@gmail.com>
2024-10-15 17:01:52 -0400

    added bootstrap styles and font awesome icons for more legibility and style.

--- controllers/todos.js
+++ controllers/todos.js
@@ -1,8 +1,7 @@
 const Todo = require('../models/Todo')
-
+ 
 module.exports = {
     getTodos: async (req,res)=>{
-        console.log(req.user)
         try{
             const todoItems = await Todo.find({userId:req.user.id})
             const itemsLeft = await Todo.countDocuments({userId:req.user.id,completed: false})
--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+
 const passport = require('passport')
 const session = require('express-session')
 const MongoStore = require('connect-mongo')(session)
 const flash = require('express-flash')
 const logger = require('morgan')
+
 const connectDB = require('./config/database')
 const mainRoutes = require('./routes/main')
 const todoRoutes = require('./routes/todos')
